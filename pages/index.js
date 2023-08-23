78b1c2e183f1241464637885da6b75dd41af2cf1
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-08-23 16:07:35 +0300

    feat: create footer component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 }
