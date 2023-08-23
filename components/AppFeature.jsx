df0098323f66c89634e7eb03d334aebf8d48fd47
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-08-23 15:20:11 +0300

    feat: created hero component

---
+++ components/AppFeature.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppFeature = () => {
+  return (
+    <div>AppFeature</div>
+  )
+}
+
+export default AppFeature
---
+++ components/PhoneFrame.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PhoneFrame = () => {
+  return (
+    <div>PhoneFrame</div>
+  )
+}
+
+export default PhoneFrame
